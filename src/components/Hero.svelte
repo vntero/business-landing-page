d9804d250f9d820c875f01dfe6ccd57f374ed580
vntero <shin.antero@gmail.com>
2024-06-21 16:53:42 +0200

    adds intro text

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -7,10 +7,14 @@
     <Header />
     <div class="flex flex-col gap-10 flex-1 items-center 
     justify-center pb-10 md:pb-14">
-        <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center">
-            <span class="text-indigo-400">Hair</span> salon 
-            <span class="text-slate-600 line-trough">for all</span><br/>
-            types <span class="text-indigo-400">of happiness</span>
+        <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold">
+            <span class="text-indigo-400">A hair</span> salon 
+            <span class="text-slate-600 line-through">is very slow</span><br/>
+            for <span class="text-indigo-400">busy people</span>
         </h2>
+        <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full">
+            Who says looking good has to be a burden? We're the stylist in your 
+            <span class="italic">neighbourhood</span>
+        </p>
     </div>
 </SectionWrapper>

1ba8978efc3de840c215cc1cf5a446986e1b8b2e
vntero <shin.antero@gmail.com>
2024-07-08 16:57:52 +0200

    finishes polishing the product card

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -35,25 +35,32 @@
     ]
 </script>
 
-<SectionWrapper id="project">
-    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14">
+<SectionWrapper id="product">
+    <div
+        class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14"
+    >
         <div class="flex flex-col gap-2">
             <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
-                Be <span class="text-indigo-400"> the best version </span> of yourself
+                Start your lifetime <span class="text-indigo-400"
+                    >gym streak</span
+                > today
             </p>
-            <h3 class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center">
-                Features for everyone
+            <h3
+                class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center"
+            >
+                Features For Everyone
             </h3>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap14 lg:gap-20 relative text-base">
             {#each productFeatures as productFeature, index}
                 <ProductCard {productFeature} {index} >
                     {#if index === 0}
-                        {:else if index === 1}
-                        {:else}
+                    Experience our fastest haircut. <span class="text-indigo-400 font-medium">Be ready to rock in 15min. </span>
+                    {:else if index === 1}
+                    Our full treatment is a guaranteed <span class="text-indigo-400 font-medium">success and relaxation.</span>
+                    {:else}
+                    Book by calling our number <span class="text-indigo-400 font-medium">or through our website.</span>
                     {/if}
                 </ProductCard>
             {/each}
         </div>
-    </div>
 </SectionWrapper>

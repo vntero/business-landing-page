ac4ac3f86eb852ffdc8870991fdd319b47ba1566
vntero <shin.antero@gmail.com>
2024-07-08 16:12:52 +0200

    adds new component - product card

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -9,6 +9,14 @@
             <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
                 Be <span class="text-indigo-400"> the best version </span> of yourself
             </p>
+            <h3 class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center">
+                Features for everyone
+            </h3>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap14 lg:gap-20 relative text-base">
+            {#each productFeatures as productFeature}
+                
+            {/each}
         </div>
     </div>
 </SectionWrapper>

663e48a63c8e2412e7472c21f66f05783b77b85c
vntero <shin.antero@gmail.com>
2024-07-08 15:10:17 +0200

    adds google reviews indicator

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -18,5 +18,17 @@
             <span class="italic">We're the stylists in your neighbourhood</span> & much much more.
         </p>
         <CTAs/>
+        <div class="flex items-center justify-center gap-2 text-base">
+            <p>4.6</p>
+            {#each [0, 1, 2, 3, 4] as index}
+                <div class="grid place-items-center relative">
+                    <i class="fa-solid fa-star opacity-0"></i>
+                    <div class={"absolute top-0 left-0 grid place-items-center " + (index === 4 ? "w-[60%] overflow-hidden" : " ")}>
+                        <i class="fa-solid fa-star text-amber-400"></i>
+                    </div>
+                </div>
+            {/each}
+            <p>500+</p>
+        </div>
     </div>
 </SectionWrapper>

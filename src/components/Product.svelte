e19c7ae1304b9fe2b4b28489d1112312e369f104
vntero <shin.antero@gmail.com>
2024-07-08 15:24:04 +0200

    adds product component section wrapper

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,7 +1,14 @@
 <script>
     import SectionWrapper from "./SectionWrapper.svelte";
+    let productFeatures=[]
 </script>
 
 <SectionWrapper id="project">
-
+    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14">
+        <div class="flex flex-col gap-2">
+            <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
+                Be <span class="text-indigo-400"> the best version </span> of yourself
+            </p>
+        </div>
+    </div>
 </SectionWrapper>

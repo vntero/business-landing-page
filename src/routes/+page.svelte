849e8a84198011a27fa791167ed24db96542e57a
vntero <shin.antero@gmail.com>
2024-07-29 09:42:34 +0200

    adds conversion component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
     import Hero from "../components/Hero.svelte"
     import Product from "../components/Product.svelte"
     import Reviews from "../components/Reviews.svelte"
+    import FAQs from "../components/FAQs.svelte"
 </script>
 
 <main class="flex flex-col">
     <Hero/>
     <Product/>
     <Reviews/>
+    <FAQs/>
 </main>
